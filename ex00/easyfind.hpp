e30f2e7f7fedfd6f0ec97e99d6dd01aed1cde822
pdrettas <pdrettas@student.42heilbronn.com>
2026-04-14 22:49:24 +0200

    FEAT(ex00): implement ft template easyfind in .hpp && write tests for vector and list

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,28 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <list>
+
+/*
+Write a function template easyfind that accepts a type T. 
+It takes two parameters:
+the first one is of type T, and the second one is an integer.
+
+Assuming T is a container of integers, 
+this function has to find the first occurrence of the second parameter 
+in the first parameter.
+
+If no occurrence is found, you can either throw an exception or return an error value
+of your choice. If you need some inspiration, analyze how standard containers behave.
+*/
+template<typename T>
+typename T::iterator easyfind(T &container, int num)
+{
+    // if failure: returns iteratorto element or end() like behavior of standard containers
+    return find(container.begin(), container.end(), num); 
+}
+
+#endif
